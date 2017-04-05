e46a2c3920f6c0071e2b539eadf67c8148494113
Stéphane Lavergne <lis@imars.com>
2017-04-05 07:23:17 -0400

    MSIE polyfill for Number.isInteger() (fixes #5)

--- assets/pyritephp.js
+++ assets/pyritephp.js
@@ -9,6 +9,15 @@ require('bootstrap');
 require('selectize');
 require('summernote');
 
+// Polyfill for MSIE
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+Number.isInteger = Number.isInteger || function(value) {
+  return typeof value === 'number'
+    && isFinite(value)
+    && Math.floor(value) === value
+  ;
+};
+
 // Our main initializations, tolerate the long format
 $().ready(function() {  // eslint-disable-line max-statements
   // Get language code from HTML tag
